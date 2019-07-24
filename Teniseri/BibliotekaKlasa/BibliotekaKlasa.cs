12aeceaf17c8b8a42f469ecf51adfefc06da0b79
MrMartingale <53217874+MrMartingale@users.noreply.github.com>
2019-07-24 12:56:07 +0200

    Add files via upload
    Exam preparation - development of record for Tennis player ATP evidence - Part I

---
+++ Teniseri/BibliotekaKlasa/BibliotekaKlasa.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibliotekaKlasa
+{
+    public enum vrsta
+    {
+        GrandSlam,
+        Masters,
+        Drugi
+    }
+    public class Turnir
+    {
+        private int godina;
+        private string naziv;
+        private int maxBrojBodova;
+        private vrsta vrsta;
+        
+        public int Godina {get => this.godina; set => godina = value;}
+        public string Naziv {get => this.naziv; set => naziv = value;}
+        public int MaxBrojBodova {get => this.maxBrojBodova; set => maxBrojBodova = value;}
+        public vrsta Vrsta {get => this.vrsta; set => vrsta = value;}
+
+        public Turnir(int godina, string naziv, int maxBrojBodova, vrsta vrsta)
+        {
+            this.godina = godina;
+            this.naziv = naziv;
+            this.maxBrojBodova = maxBrojBodova;
+            this.vrsta = vrsta;
+        }
+
+        public string Prikazi()
+        {
+            string Godina = godina.ToString();
+            Godina = Godina.Substring(Godina.Length - 2);
+           
+            return $"{vrsta.ToString()},{naziv.ToString()}, {naziv.ToString()},{Godina} ";
+
+        }
+    }
+
+    public class RezultatNaTurniru
+    {
+        private Turnir turnir;
+        private int brojOsvojenihBodova;
+
+        public Turnir Turnir { get => this.turnir; set => turnir = value;}
+        public int BrojOsvojenihBodova {get => this.brojOsvojenihBodova; set => brojOsvojenihBodova = value;}
+
+        public RezultatNaTurniru(Turnir turnir, int brojOsvojenihBodova)
+        {
+            this.turnir = turnir;
+            this.brojOsvojenihBodova = brojOsvojenihBodova;
+        }
+
+        public string Prikazi()
+        {
+            if (brojOsvojenihBodova.Equals(turnir.MaxBrojBodova))
+            {
+                return $"{turnir.ToString()},Pobedio" ;
+            }
+            return $"{turnir.ToString()}, nije pobedio ";
+        }
+        public RezultatNaTurniru UcitajRezultat(Turnir t)
+        {
+            while (true)
+            {
+                Console.WriteLine("Unesite broj osvojenih poena na turniru");
+                int brojPoena = int.Parse(Console.ReadLine());
+                if (t.Vrsta.HasFlag(vrsta.Drugi) && brojPoena > 750)
+                {
+                    throw new Exception("Turnir nije značajan, broj osvojenih poena ne može biti veći od 750!");
+                }
+                if (t.MaxBrojBodova < brojPoena)
+                {
+                    throw new Exception("Unet broj poena je veci od maksimalnog broja poena za taj turnir!");
+                }
+
+                if (brojPoena < 0)
+                {
+                    throw new Exception("Broj poena ne može biti manji od nule!");
+                }
+                if (DateTime.Now.Year == t.Godina)
+                {
+                    throw new Exception("Unet turnir mora biti u tekucoj godini!");
+                }
+
+                RezultatNaTurniru r = new RezultatNaTurniru(t, brojPoena);
+                return r;
+            }
+
+        }
+        
+    }
+}
---
+++ Teniseri/Teniseri/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BibliotekaKlasa;
+
+namespace Teniseri
+{
+    public delegate void DelPobedinik(RezultatNaTurniru r);
+
+    public class Program
+    {
+       
+        public class Osoba
+
+        {
+            private string ime;
+            private DateTime datumRodjenja;
+
+            public string Ime {get => this.ime; set => ime = value;}
+            public DateTime DatumRodjenja {get => this.datumRodjenja; set => datumRodjenja = value;}
+            
+
+            public Osoba(string ime, DateTime datumRodjenja)
+            {
+                this.ime = ime;
+                this.datumRodjenja = datumRodjenja;
+            }
+
+            public virtual string Prikazi()
+            {
+                return $"{ime},{(DateTime.Now -datumRodjenja).Ticks.ToString()}";
+            }
+        
+        }
+
+        public class Teniser:Osoba
+        {
+            public event DelPobedinik DaLiJePobedio;
+
+            private int rang;
+            private List<RezultatNaTurniru> spisakRezultata = new List<RezultatNaTurniru>();
+            public List<Turnir> spisakTurniraPobeda = new List<Turnir>();
+
+            public int Rang {get => this.rang; set => rang = value;}
+            public List<RezultatNaTurniru> SpisakRezultata {get => this.spisakRezultata; set => spisakRezultata = value;}
+
+            public Teniser(string ime, DateTime datumRodjenja, int rang, List<RezultatNaTurniru> spisakRezultata):base(ime,datumRodjenja)
+            {
+                this.rang = rang;
+                this.spisakRezultata = spisakRezultata;
+                DaLiJePobedio += new DelPobedinik(DodajPobednika);
+            }
+
+            public override string Prikazi()
+            {
+                int sum = 0;
+                int count = 0;
+                double average = 0;
+                foreach (var item in spisakRezultata)
+                {
+                    sum += item.BrojOsvojenihBodova;
+                    count++;
+                }
+                try
+                {
+                    average = sum / count;
+                }
+                catch (DivideByZeroException)
+                {
+
+                    throw new Exception("Teniser nije igrao ni na jednom turniru!");
+                }
+               
+                return $"{rang.ToString()},{base.Prikazi()},{average.ToString()}";
+            }
+             public void DodajRezultat(Teniser t, RezultatNaTurniru r, int dodatak)
+             {
+                
+                foreach (var item in t.spisakRezultata)                
+                {
+                    if (!(item.Turnir.Naziv.Equals(r.Turnir.Naziv)) && (item.Turnir.Godina == DateTime.Now.Year))
+                    {
+                        spisakRezultata.Add(r);
+                    }
+                    else
+                    {
+                        if (spisakRezultata.Contains(r))
+                        {
+                            r.BrojOsvojenihBodova += item.BrojOsvojenihBodova + dodatak;
+                            if (r.BrojOsvojenihBodova > item.Turnir.MaxBrojBodova)
+                            {
+                                item.BrojOsvojenihBodova = Math.Max(r.BrojOsvojenihBodova, item.Turnir.MaxBrojBodova);
+    
+
+                        }
+                        }
+                        
+                    }
+                }
+                
+             }
+             public void PrikaziPobednika(Teniser t)
+             {
+                foreach (var item in t.spisakRezultata)
+                {
+                    if (item.Turnir.MaxBrojBodova == item.BrojOsvojenihBodova)
+                    {
+                        Console.WriteLine($"{t.Ime},{ item.Turnir.Vrsta}");
+                    }
+                }
+             }
+            public void DodajPobednika(RezultatNaTurniru r)
+            {
+                if ((r.Turnir.MaxBrojBodova == r.BrojOsvojenihBodova) && !spisakTurniraPobeda.Contains(r.Turnir))
+                {
+                    spisakTurniraPobeda.Add(r.Turnir);
+                }
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            List<RezultatNaTurniru> spisak = new List<RezultatNaTurniru>();
+            Teniser t = new Teniser("Novak Djokovic (SRB)", new DateTime(1987, 05, 22), 1, spisak);
+            Turnir tu = new Turnir(2016,"Roland Garros",2000,vrsta.GrandSlam);
+            RezultatNaTurniru r = new RezultatNaTurniru(tu, 700);
+            RezultatNaTurniru r2 = r.UcitajRezultat(tu);
+            t.DodajRezultat(t, r2, 400);
+            t.PrikaziPobednika(t);
+        }
+    }
+}
